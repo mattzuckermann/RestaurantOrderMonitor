1e0169bb6750bc87ef1b638042c0293c251e6da7
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-09 16:32:04 -0600

    required three dependencies and added handlebars  lines of code that set path to include index.handlebars to main.handlebars file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const path = require("path");
+const connection = require("./config/connection")
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 connection.connect(function (err) {
     if (err) throw err;
