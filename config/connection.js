51439862172b4ba7a6cbd309b2105d39b67ae9a6
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-12 14:15:52 -0800

    Updated connection.js to connect mysql to heroku

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ const connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if {process.env.JAWSDB_URL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot123*",
+    database: "burgers_db"
+});
+}
 connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
