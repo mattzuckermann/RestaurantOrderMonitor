fb5a1c069a320342c730ba917bb81337ae676fad
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-12 14:33:39 -0800

    Changed Connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot123*",
-    database: "burgers_db"
-});
-
 if {process.env.JAWSDB_URL) {
   const connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
