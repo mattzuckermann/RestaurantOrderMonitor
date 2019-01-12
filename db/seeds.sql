389fb7e79b6583a5e677d2f2aaf4c76f02eca28c
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-12 10:16:58 -0800

    changed schema column name and updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Cheese");
-INSERT INTO burgers (burger_name) VALUES ("Veggie");
-INSERT INTO burgers (burger_name) VALUES ("Steak");
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ("Cheese", false);
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ("Veggie", true);
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ("Steak", false);
