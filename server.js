6a3d7256b65af6a73fdf38240d7ea1a52b3458ed
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-12 10:19:10 -0800

    Moved sql connection method to other file and added static folder

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require("express");
-const path = require("path");
-const connection = require("./config/connection")
 const exphbs = require("express-handlebars");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-connection.connect(function (err) {
-    if (err) throw err;
-    else console.log(`Your connection to ID #${connection.config.port} was successful!\n`);
-});
-
-require("./controllers/burgers_controller")(app);
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT);
