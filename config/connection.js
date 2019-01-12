c7f44ffba780b35e7d80ced8194d473dbd4997da
mczuckermann <matt.zuckermann@my.wheaton.edu>
2019-01-12 14:54:27 -0800

    Updated Connection.js to put connection variable in global scope

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
+let connection;
 
 if (process.env.JAWSDB_URL) {
-  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -11,6 +12,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
